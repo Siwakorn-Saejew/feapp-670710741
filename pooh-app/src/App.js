0cc8df2c661ae661f8fdd2ab6c0d90d8796998b2
Siwakorn Saejew <saejew_s@silpakorn.edu>
2026-08-20 05:14:37 +0000

    1

---
+++ pooh-app/src/App.js
@@ -0,0 +1,34 @@
+
+
+import ProfileCard from './Component/ProfileCard';
+import './App.css';
+
+const members = [
+  { id: 1, name: 'กรวิชญ์ ครุฑกษัตริย์ไทย', nickname: 'โอม',
+    major: 'เทคโนโลยีสารสนเทศ', favorites: ['ชาเขียว', 'แมว'] },
+  { id: 2, name: 'ศิวกร แซ่เจว', nickname: 'เจ๋ง',
+    major: 'วิศวกรรมคอมพิวเตอร์', favorites: ['กาแฟ', 'หมา'] },
+  { id: 3, name: 'ปราชญา', nickname: 'โอม',
+    major: 'วิทยาการคอมพิวเตอร์', favorites: ['น้ำผลไม้', 'นก'] },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <h1>สมาชิกกลุ่มของเรา</h1>
+      <div className="card-row">
+        {members.map((m) => (
+          <ProfileCard
+            key={m.id}
+            name={m.name}
+            nickname={m.nickname}
+            major={m.major}
+            favorites={m.favorites}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
